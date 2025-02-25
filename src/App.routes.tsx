221ed584e2dc267a625ae2f52c70115ac2663079
TrueVinci <kattamuri1506@gmail.com>
2025-02-25 04:32:47 -0600

    Update App.routes.tsx

--- src/App.routes.tsx
+++ src/App.routes.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import { ProjectDetail } from './pages/ProjectDetail';
+import { ThemeProvider } from './ThemeContext';
 
 export const AppRoutes = () => {
+  const baseUrl = import.meta.env.VITE_BASE_URL || '/'; // Default to '/' if not set
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/projects/:id" element={<ProjectDetail />} />
-      </Routes>
+    <BrowserRouter basename={baseUrl}>
+      <ThemeProvider>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/projects/:id" element={<ProjectDetail />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   );
 };
